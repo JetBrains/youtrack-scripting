4f8e241fac8f64297dde3254d9cfcd961798a04c
Alexander Anisimov <Alexander.Anisimov@jetbrains.com>
2017-08-16 14:14:36 +0200

    More ES6 code style

--- src/download.js
+++ src/download.js
@@ -1,22 +1,22 @@
-var fs = require('fs');
-var resolve = require('url').resolve;
-var net = require('../lib/net/index');
-var tmpdir = require('../lib/fs/tmpdir');
+const fs = require('fs');
+const resolve = require('url').resolve;
+const net = require('../lib/net/index');
+const tmpdir = require('../lib/fs/tmpdir');
 
-var request = net.request;
-var sign = net.sign;
+const request = net.request;
+const sign = net.sign;
 
 module.exports = function(config, workflowName) {
   if (!workflowName) throw new Error('Workflow name should be defined');
 
-  var req = request(sign(resolve(config.host, '/rest/workflow/distributive/' + workflowName), config.token),
+  const req = request(sign(resolve(config.host, '/rest/workflow/distributive/' + workflowName), config.token),
     (error) => {
       if (error) throw error;
     }
   );
 
   req.on('response', (res) => {
-    var zip = fs.createWriteStream(tmpdir(getZipName(workflowName)));
+    const zip = fs.createWriteStream(tmpdir(getZipName(workflowName)));
     res.pipe(zip);
   });
 
