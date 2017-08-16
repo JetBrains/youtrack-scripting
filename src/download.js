43c1cae9224f49ceffa95f766a48abc922421da8
Alexander Anisimov <Alexander.Anisimov@jetbrains.com>
2017-08-16 18:19:41 +0200

    chore(list): more about download

--- src/download.js
+++ src/download.js
@@ -3,26 +3,52 @@ const resolve = require('url').resolve;
 const net = require('../lib/net/index');
 const tmpdir = require('../lib/fs/tmpdir');
 
+const load = require('../src/load');
+
 const request = net.request;
 const sign = net.sign;
 
-module.exports = function(config, workflowName) {
-  if (!workflowName) throw new Error('Workflow name should be defined');
+/**
+ * Get by `id` until https://youtrack.jetbrains.com/issue/JT-42979 is not fixed
+ */
+function getId(config, workflowName, fn) {
+  load(config, list => {
+    const targetWorkflow = (list || []).find(function(item) {
+      return item.name === workflowName;
+    });
 
-  const req = request(sign(resolve(config.host, '/rest/workflow/distributive/' + workflowName), config.token),
-    (error) => {
-      if (error) throw error;
+    if (!targetWorkflow || !targetWorkflow.id) {
+      throw new Error('Can\'t get requested workflow id');
     }
-  );
 
-  req.on('response', (res) => {
-    const zip = fs.createWriteStream(tmpdir(getZipName(workflowName)));
-    res.pipe(zip);
+    fn && fn(targetWorkflow.id);
   });
+}
 
-  return req;
+function download(config, workflowName) {
+  if (!workflowName) {
+    throw new Error('Workflow id/name should be defined');
+  }
+
+  return getId(config, workflowName, id => {
+    const req = request(
+      sign(resolve(config.host, '/workflowDistributive/' + id), config.token),
+      (downloadError) => {
+        if (downloadError) throw downloadError;
+      }
+    );
+
+    req.on('response', reponse => {
+      const zip = fs.createWriteStream(tmpdir(getZipName(workflowName)));
+      reponse.pipe(zip);
+    });
+
+    return req;
+  });
 
   function getZipName(workflowName) {
     return 'youtrack-workflow-' + workflowName + '-' + Date.now() + '.zip';
   }
-};
+}
+
+module.exports = download;
