40f5f6db50aaa1b055734f9b9fc52a4aa5a744fa
Maksim Ryzhikov <rv.maksim@gmail.com>
2017-08-21 17:42:15 +0300

    fix: create new workflow if it does not exist

--- src/cli/upload.js
+++ src/cli/upload.js
@@ -18,27 +18,44 @@ module.exports = function(config, workflowDir) {
   }
 
   zipfolder(path.resolve(config.cwd, workflowDir), zipPath, (error, zip) => {
-    if (error) return exit(error);
+    if (error) {
+      return exit(error);
+    }
 
-    var content = JSON.stringify({
-      base64Content: fs.readFileSync(zip.path).toString('base64')
-    });
+    return updateWorkflow();
 
-    var message = new HttpMessage(resolve(config.host, '/api/admin/workflows/' + workflowName));
-    message.method = 'POST';
-    message = config.token ? HttpMessage.sign(message, config.token) : message;
-    message.headers['Content-Type'] = 'application/json';
-    message.headers['Content-Length'] = content.length;
+    function updateWorkflow(isCreate) {
+      var content = JSON.stringify({
+        name: isCreate ? workflowName : undefined,
+        base64Content: fs.readFileSync(zip.path).toString('base64')
+      });
 
-    var req = request(message, (error) => {
-      if (error) return exit(error);
+      var message = new HttpMessage(resolve(config.host, '/api/admin/workflows/' + (isCreate ? '' : workflowName)));
+      message.method = 'POST';
+      message = config.token ? HttpMessage.sign(message, config.token) : message;
+      message.headers['Content-Type'] = 'application/json';
+      message.headers['Content-Length'] = content.length;
 
-      console.log(i18n('Workflow "' + workflowName + '" is uploaded'));
-    });
+      var req = request(message, (error) => {
+        if (error && error.statusCode === 404 && !isCreate) { // Try create new workflow
+          return updateWorkflow(true);
+        }
 
-    req.write(content);
-    req.end();
-    return req;
+        if (error) {
+          return exit(error);
+        }
+
+        if (isCreate) {
+          console.log(i18n('Workflow "' + workflowName + '" is created'));
+        } else {
+          console.log(i18n('Workflow "' + workflowName + '" is uploaded'));
+        }
+      });
+
+      req.write(content);
+      req.end();
+      return req;
+    }
   });
 
   function generateZipName(workflowDir) {
